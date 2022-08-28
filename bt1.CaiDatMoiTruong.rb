79042d7280a9e20464de933557ef91e8bcb80e4a
thuyhang <thuyhangfr01@gmail.com>
2022-08-28 12:36:46 +0700

    first commit

---
+++ bt1.CaiDatMoiTruong.rb
@@ -0,0 +1,17 @@
+puts "Xin chào Cao Thị Thúy Hằng - 1911505310118!"
+
+puts "--- Bài tập 1.1 ---"
+print "Nhập 1 số: "
+num = gets
+puts "=> Số bạn vừa nhập là: #{num}" 
+
+puts "--- Bài tập 1.2 ---"
+print "Nhập 2 số: "
+num1 = gets
+num2 = gets
+print "=> 2 số bạn vừa nhập là: #{num1} và #{num2}"
+if num1 > num2
+    print "Giá trị lớn hơn là: #{num1}"
+else
+    print "Giá trị lớn hơn là: #{num2}"
+end
