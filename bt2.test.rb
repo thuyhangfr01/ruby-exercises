48e9f7d788b1f8441f916586002d24cb5c8aaae0
thuyhang <thuyhangfr01@gmail.com>
2022-09-16 11:21:10 +0700

    Hang commit at 16/09/2022

---
+++ bt2.test.rb
@@ -0,0 +1,12 @@
+puts "Xin chào Cao Thị Thúy Hằng - 1911505310118!"
+
+puts "--- Bài tập 1 ---"
+array = [12, 78, 2]
+# print "Nhập 1 số: "
+# num = gets
+max = array.max()
+min = array.min()
+avg = array.sum / array.size.to_f
+puts "#{array}.max() = #{max}"
+puts "#{array}.min() = #{min}"
+puts "#{avg}"
